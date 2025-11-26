99ebbc142300812c9dfd9fbcb8d318a0fd92fa60
Konsolev-Loger <ocuperusha@gmail.com>
2025-11-26 15:59:29 +0300

    fix seed adn create public

--- server/db/migrations/7-create-category.js
+++ server/db/migrations/7-create-category.js
@@ -1,45 +1,29 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Materials', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      category_id: {
-        type: Sequelize.INTEGER,
-      },
       name: {
-        type: Sequelize.STRING,
-      },
-      title: {
-        type: Sequelize.STRING,
-      },
-      price: {
-        type: Sequelize.INTEGER,
-      },
-      unit_m2: {
-        type: Sequelize.DECIMAL,
-      },
-      is_popular: {
-        type: Sequelize.BOOLEAN,
-      },
-      img: {
         type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
       },
     });
   },
   async down(queryInterface) {
-    await queryInterface.dropTable('Materials');
+    await queryInterface.dropTable('Categories');
   },
 };
