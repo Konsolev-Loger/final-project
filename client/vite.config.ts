4f167ce6aae218d51ca686d051695d30f4b26eb1
orrey6three <orstudiocorp@gmail.com>
2025-11-26 16:21:54 +0500

    install TailwindCSS

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,12 @@ import type { AliasOptions } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
+import tailwindcss from '@tailwindcss/vite';
 const root = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': root,
