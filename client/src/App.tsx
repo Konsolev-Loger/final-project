c069d1193bba3e35a3fee023c6ab1579b8e05473
Rakiiia <Rakiiia@mail.ru>
2025-11-27 19:17:53 +0300

    Zhenya_skazal_vse_zalupa

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RoomsPage from './pages/RoomsPage';
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/rooms" element={<RoomsPage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
