9b93916baf79dc4257a374cfa1524149910aac5d
Rakiiia <Rakiiia@mail.ru>
2025-11-28 11:44:55 +0300

    change_bd_castomroom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import Register from './pages/Register';
 import { useAppDispatch } from './store/hooks';
 import { refreshTokensThunk } from './entities/user/api/UserApi';
 import RoomsPage from './pages/RoomsPage';
+import CalculatePage from './pages/CalculatePage';
 
 const queryClient = new QueryClient();
 
@@ -28,16 +29,16 @@ export default function App(): React.JSX.Element {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* <Route path="/category" element={<Category />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/rooms" element={<RoomsPage />} />
+            <Route path="/calculate" element={<CalculatePage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
